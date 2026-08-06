7f3c2dccb4a468af06853b5256821d16b5e465b8
Sameer Ansari <Samiranshari12786@gmail.com>
2026-08-06 22:45:58 +0545

    Support custom S3 endpoint URL for Cloudflare R2

--- locker/settings.py
+++ locker/settings.py
@@ -128,7 +128,11 @@ if os.environ.get("AWS_ACCESS_KEY_ID"):
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-1")
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    
+    if os.environ.get("AWS_S3_ENDPOINT_URL"):
+        AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
+    
+    AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com")
     AWS_S3_FILE_OVERWRITE = False
     
     STORAGES["default"] = {
